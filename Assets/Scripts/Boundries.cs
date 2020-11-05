5a7815a4371c47043596d72dd320540b0e3e94f3
alonp13 <alonp13@gmail.com>
2020-11-05 10:06:43 +0200

    Check

--- Assets/Scripts/Boundries.cs
+++ Assets/Scripts/Boundries.cs
@@ -16,7 +16,7 @@ public class Boundries : MonoBehaviour
 
     float bottomLimit;
 
-    // Start is called before the first frame update
+    // Start is called before the first frame update.
     void Start()
     {
         GameObject streetObject = GameObject.Find("Street");
